7fa855a79cf17d11f298121d2f12e93413f89492
Maurício Witter <46854467+rwietter@users.noreply.github.com>
2022-02-21 16:45:23 -0300

    remove db props (#1)

--- components/contentItem/contentItem.tsx
+++ components/contentItem/contentItem.tsx
@@ -1,45 +1,47 @@
 import { FC } from 'react';
 
-import { LinkProps } from '../../db/@types';
 import { Heading } from '../styles/Heading';
 import { PageLink } from '../styles/PageLink';
 import { PageSectionItem } from '../styles/PageSectionItem';
 import { Tooltip } from '../tooltip';
 
 interface ContentProps {
-  pageIndex: string[];
-  pageContent: any;
+	pageIndex: string[];
+	pageContent: any;
 }
 
-const ContentItem: FC<ContentProps> = ({ pageIndex, pageContent }) => {
-  return (
-    <>
-      {pageIndex?.map((value: string) => {
-        return (
-          <PageSectionItem key={value}>
-            <Heading id={value.toLowerCase()} type="h3" weight={400}>{value}</Heading>
-            <ul>
-              {pageContent ? (
-                pageContent[value]?.map(({ name, url }: LinkProps, idx: number) => {
-                  return (
-                    <li key={idx}>
-                      <Tooltip message={url}>
-                        <PageLink href={url} target="_blank">
-                          {name}
-                        </PageLink>
-                      </Tooltip>
-                    </li>
-                  );
-                })
-              ) : (
-                <div />
-              )}
-            </ul>
-          </PageSectionItem>
-        );
-      })}
-    </>
-  );
+export type LinkProps = {
+	name: string;
+	url: string;
 };
+
+const ContentItem: FC<ContentProps> = ({ pageIndex, pageContent }) => (
+	<div>
+		{pageIndex?.map((value: string) => (
+			<PageSectionItem key={value}>
+				<Heading id={value.toLowerCase()} type="h3" weight={400}>
+					{value}
+				</Heading>
+				<ul>
+					{pageContent ? (
+						pageContent[value]?.map(
+							({ name, url }: LinkProps, idx: number) => (
+								<li key={idx}>
+									<Tooltip message={url}>
+										<PageLink href={url} target="_blank">
+											{name}
+										</PageLink>
+									</Tooltip>
+								</li>
+							),
+						)
+					) : (
+						<div />
+					)}
+				</ul>
+			</PageSectionItem>
+		))}
+	</div>
+);
 
 export { ContentItem };
